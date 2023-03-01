ac1e60c9a4afdbcb7e7d8bd740038aed70720697
Zohirjon <example@mail.com>
2023-03-01 15:58:42 +0500

    crate clock project

---
+++ main.js
@@ -0,0 +1,15 @@
+const d = 6;
+const hr = document.querySelector("#hr");
+const mn = document.querySelector("#mn");
+const sc = document.querySelector("#sc");
+
+setInterval(() => {
+  let day = new Date();
+  let hh = day.getHours() * 30;
+  let mm = day.getMinutes() * d;
+  let ss = day.getSeconds() * d;
+
+  hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+  mn.style.transform = `rotateZ(${mm}deg)`;
+  sc.style.transform = `rotateZ(${ss}deg)`;
+});
